e7fc9b413b00dd2da96c74abdd5f22d6b08551b8
Joshua Nelson <jyn514@gmail.com>
2020-08-14 23:33:05 -0400

    Split up `saltwater_codegen` into a separate crate

--- saltwater-parser/lex/cpp.rs
+++ saltwater-parser/lex/cpp.rs
@@ -46,7 +46,7 @@ use crate::Files;
 ///
 /// Here is the example for `PreProcessor::new()` using the builder:
 /// ```
-/// use saltwater::PreProcessorBuilder;
+/// use saltwater_parser::PreProcessorBuilder;
 ///
 /// let cpp = PreProcessorBuilder::new("int main(void) { char *hello = \"hi\"; }\n").filename("example.c").build();
 /// for token in cpp {
@@ -122,7 +122,7 @@ impl<'a> PreProcessorBuilder<'a> {
 /// Examples:
 ///
 /// ```
-/// use saltwater::PreProcessor;
+/// use saltwater_parser::PreProcessor;
 ///
 /// let cpp = PreProcessor::new("int main(void) { char *hello = \"hi\"; }\n", "example.c", false, vec![], Default::default());
 /// for token in cpp {
